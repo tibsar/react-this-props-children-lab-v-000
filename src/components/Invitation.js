0e268532d6749490ec9c91f50b0e4fd55df8690a
Sara Tibbetts <sxt4866@rit.edu>
2018-03-24 18:25:36 -0400

    finishes lab

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,15 @@
-// Code Invitation Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Invitation extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        <p>{this.props.children}</p>
+      </div>
+    );
+  }
+}
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,20 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ThemedDecorations extends React.Component{
+  render(){
+    const childrenWithClassName = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      });
+    });
+
+    return (
+      <div>
+        {childrenWithClassName}
+      </div>
+    );
+  }
+}
+
+export default ThemedDecorations;
